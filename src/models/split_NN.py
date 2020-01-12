0c44bbc93ee656554919c6919ab379a67b811446
Riley <rileywhite89@gmail.com>
2020-01-12 00:58:29 -0500

    fixed input shape and increased split NN size again

--- src/models/split_NN.py
+++ src/models/split_NN.py
@@ -22,25 +22,27 @@ class split_NN():
         input1 = keras.Input(batch_shape=(params[index]["batch_size"], params[index]["max_code_length"],
                              params[index]['dataset'].len_encoding),
                              name='input_1')
+        input1 = layers.Flatten()(input1)
         input2 = keras.Input(batch_shape=(params[index]["batch_size"], params[index]["max_code_length"],
                              params[index]['dataset'].len_encoding),
                              name='input_2')
+        input2 = layers.Flatten()(input2)
 
         dense1 = layers.Dense(1024, activation='relu', name='dense1_1')(input1)
         dense1 = keras.layers.Dropout(params[index]['dropout'])(dense1)
         dense1 = layers.Dense(1024, activation='relu', name='dense1_2')(dense1)
         dense1 = keras.layers.Dropout(params[index]['dropout'])(dense1)
-        dense1 = layers.Dense(512, activation='relu', name='dense1_3')(dense1)
+        dense1 = layers.Dense(1024, activation='relu', name='dense1_3')(dense1)
 
         dense2 = layers.Dense(1024, activation='relu', name='dense2_1')(input2)
         dense2 = keras.layers.Dropout(params[index]['dropout'])(dense2)
         dense2 = layers.Dense(1024, activation='relu', name='dense2_2')(dense2)
         dense2 = keras.layers.Dropout(params[index]['dropout'])(dense2)
-        dense2 = layers.Dense(512, activation='relu', name='dense2_3')(dense2)
+        dense2 = layers.Dense(1024, activation='relu', name='dense2_3')(dense2)
 
 
         concat = layers.concatenate([dense1, dense2])
-        dense1 = Dense(1024, activation='relu', name="dense3_1")(concat)
+        dense1 = Dense(2024, activation='relu', name="dense3_1")(concat)
         dense2 = Dense(1024, activation='relu', name="dense3_2")(dense1)
         dense3 = Dense(512, activation='relu', name="dense3_3")(dense2)
         dense4 = Dense(256, activation='relu', name="dense3_4")(dense3)
