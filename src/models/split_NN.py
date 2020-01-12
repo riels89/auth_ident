b6c04c606c9a1b768f827f0d5444d18a92bebf9c
Riley <rileywhite89@gmail.com>
2020-01-12 00:38:09 -0500

    fixed splitNN layer names

--- src/models/split_NN.py
+++ src/models/split_NN.py
@@ -26,24 +26,24 @@ class split_NN():
                              params[index]['dataset'].len_encoding),
                              name='input_2')
 
-        dense1 = layers.Dense(1024, activation='relu', name='dense_1')(input1)
+        dense1 = layers.Dense(1024, activation='relu', name='dense1_1')(input1)
         dense1 = keras.layers.Dropout(params[index]['dropout'])(dense1)
-        dense1 = layers.Dense(1024, activation='relu', name='dense_2')(dense1)
+        dense1 = layers.Dense(1024, activation='relu', name='dense1_2')(dense1)
         dense1 = keras.layers.Dropout(params[index]['dropout'])(dense1)
-        dense1 = layers.Dense(1024, activation='relu', name='dense_3')(dense1)
+        dense1 = layers.Dense(1024, activation='relu', name='dense1_3')(dense1)
 
-        dense2 = layers.Dense(1024, activation='relu', name='dense_1')(input2)
+        dense2 = layers.Dense(1024, activation='relu', name='dense2_1')(input2)
         dense2 = keras.layers.Dropout(params[index]['dropout'])(dense2)
-        dense2 = layers.Dense(1024, activation='relu', name='dense_2')(dense2)
+        dense2 = layers.Dense(1024, activation='relu', name='dense2_2')(dense2)
         dense2 = keras.layers.Dropout(params[index]['dropout'])(dense2)
-        dense2 = layers.Dense(1024, activation='relu', name='dense_3')(dense2)
+        dense2 = layers.Dense(1024, activation='relu', name='dense2_3')(dense2)
 
 
         concat = layers.concatenate([dense1, dense2])
-        dense1 = Dense(2048, activation='relu', name="dense_1")(concat)
-        dense2 = Dense(1024, activation='relu', name="dense_2")(dense1)
-        dense3 = Dense(512, activation='relu', name="dense_3")(dense2)
-        dense4 = Dense(256, activation='relu', name="dense_4")(dense3)
+        dense1 = Dense(2048, activation='relu', name="dense3_1")(concat)
+        dense2 = Dense(1024, activation='relu', name="dense3_2")(dense1)
+        dense3 = Dense(512, activation='relu', name="dense3_3")(dense2)
+        dense4 = Dense(256, activation='relu', name="dense3_4")(dense3)
 
         outputs = Dense(1, activation='sigmoid', name='predictions')(dense4)
 
