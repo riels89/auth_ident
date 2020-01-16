f32cd97eba1e8709aec29f7a6e4984eb50732ef6
Riley <rileywhite89@gmail.com>
2020-01-16 10:01:45 -0500

    added EXP4 to test optimizations

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -151,4 +151,4 @@ class trainer:
 # trainer(cnn_lstm(), "first_runs", 1, "11-25-19").train()
 # trainer(split_cnn(), "smaller_cnn", 4, "1-11-20").train()
 # trainer(largeNN(), "first_runs", 1, "12-10-19").train()
-trainer(split_NN(), "prefetch-5", 3, "1-16-20").train()
+trainer(split_NN(), "test_optimizations", 4, "1-16-20").train()
