1b70f9cae4be7f911c540b36f6943ab1fa6a85c2
Riley <rileywhite89@gmail.com>
2020-05-11 22:20:36 -0400

    WIP byline

--- src/misc_scripts/test_byline.py
+++ src/misc_scripts/test_byline.py
@@ -2,8 +2,8 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from src.preprocessing.by_line_dataset import by_line_dataset
-dataset = by_line_dataset(max_lines=1000, max_line_length=120, batch_size=64, binary_encoding=False)
+dataset = by_line_dataset(max_lines=50, max_line_length=120, batch_size=64, binary_encoding=False)
 train, val = dataset.get_dataset()
 
 for batch in train:
-	print("batch"+str(batch[0]))
+	print("batch"+str(batch.shape))
