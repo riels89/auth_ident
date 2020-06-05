fab446f20ba1ee579b978a6de4526370c02959d2
logangregorym <logangregorym@gmail.com>
2020-06-05 12:46:09 -0400

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+	echo "Usage: ./train.sh <model_name> <exp_number> <exp_name> <date(mm-dd-yyy)>"
+fi
+
+cd src/models/
+cp train_model.py temp.py
+echo "from $1 import $1" >> temp.py
+echo "trainer($1(), \"$2\", $3, \"$4\").train()" >> temp.py
+
+cd ../../
+python src/models/temp.py &> "$1-EXP$2"
+rm src/models/temp.py
