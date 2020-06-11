f5d8543cf496b2cb652f0e2f623e00b2c1ded17c
Logan <logangregorym@gmail.com>
2020-06-11 10:17:54 -0400

    updated train script

--- train.sh
+++ train.sh
@@ -2,13 +2,14 @@
 
 if [ "$#" -ne 4 ]; then
 	echo "Usage: ./train.sh <model_name> <exp_number> <exp_name> <date(mm-dd-yyy)>"
+	exit
 fi
 
 cd src/models/
 cp train_model.py temp.py
 echo "from $1 import $1" >> temp.py
-echo "trainer($1(), \"$2\", $3, \"$4\").train()" >> temp.py
+echo "trainer($1(), \"$3\", $2, \"$4\").train()" >> temp.py
 
 cd ../../
-python src/models/temp.py &> "$1-EXP$2"
+python src/models/temp.py &> "$1-EXP$2.out"
 rm src/models/temp.py
