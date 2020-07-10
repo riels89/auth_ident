daa69e3b12362a62f17efc90318c67b3b20f033b
Logan <logangregorym@gmail.com>
2020-07-10 15:19:32 -0400

    Added script to setup datasets

---
+++ gen_hdf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python src/data_processing_expt/save_hdf.py --val-test-split .05 --out data/loaded/python &
+python src/data_processing_expt/save_hdf.py --extensions cxx cc cpp c++ C --val-test-split .05 --out data/loaded/cpp &
+python src/data_processing_expt/save_hdf.py --extensions c cxx cc cpp c++ C --val-test-split .05 --out data/loaded/c_cpp &
+python src/data_processing_expt/save_hdf.py --extensions java --val-test-split .05 --out data/loaded/java &
+python src/data_processing_expt/save_hdf.py --extensions c cxx cc cpp c++ C h hh H hxx hpp h++ --val-test-split .05 --out data/loaded/c_cpp_h
