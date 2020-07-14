58c98dd50d014e2dbff9e97c5a8a429e2e6e1355
logangregorym <logangregorym@gmail.com>
2020-07-14 11:13:30 -0400

    Update pairs_dataset_demo.py

--- src/data_processing_expt/pairs_dataset_demo.py
+++ src/data_processing_expt/pairs_dataset_demo.py
@@ -26,11 +26,11 @@ if __name__ == "__main__":
     print("reading hdf...")
     df = pd.read_hdf('/home/spragunr/nobackup/pyfull.hdf')
     print("building generator...")
-    pg = pairs_generator.PairGen(df, crop_length=10, samples_per_epoch=100)
+    pg = pairs_generator.PairGen(df, crop_length=1200, samples_per_epoch=1000000)
 
     dataset = tf.data.Dataset.from_generator(
         pg.gen,
-        (tf.string, tf.string, tf.int32))
+        ({"input_1": tf.uint8, "input_2": tf.uint8}, tf.bool))
 
     print(list(dataset.take(3).as_numpy_iterator()))
 
