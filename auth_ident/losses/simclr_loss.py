2b6992b63b1d679441739af92139bbb2f8a11a0e
Riley White <rileywhite89@gmail.com>
2020-09-25 14:31:41 -0400

    Adds var to choose method type.
    Also precomputes something for method 2

--- auth_ident/losses/simclr_loss.py
+++ auth_ident/losses/simclr_loss.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 class SimCLRLoss():
-
-    def __init__(self, 
+    def __init__(self,
                  batch_size,
                  criterion=tf.nn.sparse_softmax_cross_entropy_with_logits,
-                 temperature=1.0): 
+                 temperature=1.0,
+                 method=2):
         """
             Creates a keras loss to compute the contrastive loss
             for a batch.
@@ -35,9 +35,13 @@ class SimCLRLoss():
         self.criterion = criterion
         self.temperature = temperature
         self.batch_size = batch_size
+        self.method = method
+
+        # Method 1 pre compute
         self.neg_mask = self._get_neg_mask(self.batch_size)
 
-        bottom_half = tf.range(self.batch_size, 2 * self.batch_size)[np.newaxis]
+        bottom_half = tf.range(self.batch_size,
+                               2 * self.batch_size)[np.newaxis]
         left_corner = tf.range(self.batch_size)[np.newaxis]
 
         self.index_l = tf.concat([bottom_half, left_corner], axis=0)
@@ -45,6 +49,10 @@ class SimCLRLoss():
 
         self.index_r = tf.concat([left_corner, bottom_half], axis=0)
         self.index_r = tf.transpose(self.index_r, [1, 0])
+
+        # Method 2 pre compute
+        self.inv_masks = np.invert(np.identity(batch_size, np.bool))
+        self.labels = np.arange(batch_size)
 
         self.__name__ = "simclr"
 
@@ -69,54 +77,59 @@ class SimCLRLoss():
         # get actual batch_size and compare with given batch_size
         # if different recompute neg_mask
         b = int(y_pred.shape[0] / 2)
-        if b != self.batch_size:
-            curr_neg_mask = self._get_neg_mask(b)
-        else:
-            curr_neg_mask = self.neg_mask
         y_norm = tf.math.l2_normalize(y_pred, axis=1)
 
-        sim = tf.matmul(y_norm, y_norm, transpose_b=True)
-        pos = np.zeros((2 * b, 2 * b))
+        if self.method == 1:
+            if b != self.batch_size:
+                curr_neg_mask = self._get_neg_mask(b)
+            else:
+                curr_neg_mask = self.neg_mask
 
-        pos_l = tf.gather_nd(sim, self.index_l)
-        # TODO: We don't need to compute the righ side separately do we?
-        pos_r = tf.gather_nd(sim, self.index_r)
-        
-        pos = tf.concat([pos_l, pos_r], axis=0)[:, np.newaxis]
-        neg = tf.reshape(tf.boolean_mask(sim, curr_neg_mask), (2 * b, -1))
+            sim = tf.matmul(y_norm, y_norm, transpose_b=True)
+            pos = np.zeros((2 * b, 2 * b))
 
-        logits = tf.concat([pos, neg], axis=1)
-        logits /= self.temperature
+            pos_l = tf.gather_nd(sim, self.index_l)
+            # TODO: We don't need to compute the righ side separately do we?
+            pos_r = tf.gather_nd(sim, self.index_r)
 
-        labels = tf.zeros(2 * b, dtype=tf.int32)
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits)
-        loss = tf.math.reduce_sum(loss) / float(2 * b)
-        print("LOSS:", loss)
+            pos = tf.concat([pos_l, pos_r], axis=0)[:, np.newaxis]
+            neg = tf.reshape(tf.boolean_mask(sim, curr_neg_mask), (2 * b, -1))
 
-        # METHOD 2
-        v1, v2 = tf.split(y_norm, 2, 0)
+            logits = tf.concat([pos, neg], axis=1)
+            logits /= self.temperature
 
-        labels = tf.range(b)
-        masks = np.invert(np.identity(b, np.bool))
+            labels = tf.zeros(2 * b, dtype=tf.int32)
+            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                labels, logits)
+            loss = tf.math.reduce_sum(loss) / float(2 * b)
+            return loss
 
-        # a = i and b = j, this makes it easier to read
-        logits_aa = tf.matmul(v1, v1, transpose_b=True) / self.temperature
-        logits_aa = tf.reshape(logits_aa[masks], (b, b - 1))
+        else:
 
-        logits_bb = tf.matmul(v2, v2, transpose_b=True) / self.temperature
-        logits_bb = tf.reshape(logits_bb[masks], (b, b - 1))
+            # METHOD 2
+            v1, v2 = tf.split(y_norm, 2, 0)
 
-        logits_ab = tf.matmul(v1, v2, transpose_b=True) / self.temperature
-        # TODO: We don't need to compute the righ side separately do we?
-        logits_ba = tf.matmul(v2, v1, transpose_b=True) / self.temperature
-    
-        loss_a = tf.nn.sparse_softmax_cross_entropy_with_logits(
-            labels, tf.concat([logits_ab, logits_aa], 1))
-        loss_b = tf.nn.sparse_softmax_cross_entropy_with_logits(
-            labels, tf.concat([logits_ba, logits_bb], 1))
-        loss = loss_a + loss_b
+            labels = self.labels[:b] 
+            masks = self.inv_masks[:b, :b]
 
-        return tf.math.reduce_sum(loss) / float(2 * b)
+            # a = i and b = j, this makes it easier to read
+            logits_aa = tf.matmul(v1, v1, transpose_b=True) / self.temperature
+            logits_aa = tf.reshape(logits_aa[masks], (b, b - 1))
+
+            logits_bb = tf.matmul(v2, v2, transpose_b=True) / self.temperature
+            logits_bb = tf.reshape(logits_bb[masks], (b, b - 1))
+
+            logits_ab = tf.matmul(v1, v2, transpose_b=True) / self.temperature
+            # TODO: We don't need to compute the righ side separately do we?
+            logits_ba = tf.matmul(v2, v1, transpose_b=True) / self.temperature
+
+            loss_a = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                labels, tf.concat([logits_ab, logits_aa], 1))
+            loss_b = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                labels, tf.concat([logits_ba, logits_bb], 1))
+            loss = loss_a + loss_b
+
+            return tf.math.reduce_sum(loss) / float(2 * b)
 
     def _get_neg_mask(self, batch_size):
         """
@@ -133,6 +146,6 @@ class SimCLRLoss():
         diag = np.eye(2 * batch_size)
         l1 = np.eye((2 * batch_size), 2 * batch_size, k=-batch_size)
         l2 = np.eye((2 * batch_size), 2 * batch_size, k=batch_size)
-        mask = l1 + l2 + diag 
+        mask = l1 + l2 + diag
         mask = (1 - mask).astype(bool)
-        return mask 
+        return mask
